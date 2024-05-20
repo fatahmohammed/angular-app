f55f51e904f8d38624389a513b90f01420d85a03
FATAH Mohammed <fatah.mohammed.in@gmail.com>
2024-05-21 00:42:47 +0200

    ffffffffff255

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-let express = require(express);
+let express = require("express");
+
 let app = express();
+
 app.use(express.static(__dirname + "/dist/mon-application-angular"));
 app.get("/*", (req, resp) => {
   resp.sendFile(__dirname + "/dist/mon-application-angular/index.html");
